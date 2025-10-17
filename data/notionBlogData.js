ed2fe3ec80408b305e4d30308bac9725d7b59e8d
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-10-17 14:55:53 -0400

    create functions needed to push Notion objects

---
+++ data/notionBlogData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    "id": "28e0721d-8085-80fc-80f6-f4f80b9920f5",
+    "tag": "",
+    "title": "The Beauty of Boring Code",
+    "summary": "",
+    "link": "#",
+    "thumbnail": ""
+  },
+  {
+    "id": "28e0721d-8085-80fb-9638-f4b2d629db55",
+    "tag": "",
+    "title": "When the Tutorial Ends",
+    "summary": "",
+    "link": "#",
+    "thumbnail": ""
+  },
+  {
+    "id": "28e0721d-8085-807c-91ba-c61147aca9ab",
+    "tag": "",
+    "title": "Accessibility Is a Mindset, Not a Checklist",
+    "summary": "",
+    "link": "#",
+    "thumbnail": ""
+  }
+];
--- fetchNotionData.js
+++ fetchNotionData.js
@@ -1,10 +1,12 @@
 import {config} from 'dotenv';
 config();
+import fs from "fs/promises";
+import {Client} from '@notionhq/client';
+//import {NotionToMarkdown} from 'notion-to-md';
 const NOTION_PAGE_ID = process.env.NOTION_PAGE_ID;
 const NOTION_API_KEY = process.env.NOTION_API_KEY;
 
-import {Client} from '@notionhq/client';
-import {NotionToMarkdown} from 'notion-to-md';
+
 
 //initilizing the Notion client
 const notion = new Client({
@@ -12,16 +14,66 @@ const notion = new Client({
 })
 
 //passing the notion client to the option
-const n2m = new NotionToMarkdown({notionClient:notion});
+//const n2m = new NotionToMarkdown({notionClient:notion});
 
+async function getChildPages(pageId){
+    try{
+        const response = await notion.blocks.children.list({block_id: pageId});
+        
+        
+        const pagesArray = response.results.filter((page)=>page.type ==='child_page').map((page)=>{
+            return {
+                id:page.id,
+                tag:"",
+                title:page.child_page.title,
+                summary:"",
+                link:"#",
+                thumbnail:""
+            };
+        });
+
+        return pagesArray;
+
+    } catch(err){
+        console.error("Error fetching child pages:", err);
+    }
+    
+
+}
+async function savePagesToFile(){
+    try{
+        const parentPageId = NOTION_PAGE_ID;
+        const childPages = await getChildPages(parentPageId);
+        const jsContent = `export default ${JSON.stringify(childPages,null,2)};\n`;
+        await fs.writeFile("./data/notionBlogData.js", jsContent);
+        console.log("✅ pages.js successfully written!");
+    } catch{
+        console.error("Error writing file:", err);
+    }
+}
+//savePagesToFile();
 async function getPageContentById(pageId){
-    const mdblocks= await n2m.pageToMarkdown(pageId);
-    const mdString = n2m.toMarkdownString(mdblocks);
-    return (
-        [mdblocks[1].children[0],mdblocks[1].children[1],mdblocks[1].children[2],mdblocks[1].children[3],
-    mdblocks[1].children[4],mdblocks[1].children[5]]
-    ); 
-   // return mdString.parent;
+    try{
+        const childPage = await notion.blocks.children.list({block_id: pageId});
+        return childPage;
+    }
+    catch(err){
+        console.error("Error fetching page content:", err);
+    }
+
+}
+async function getPageTitleById(pageId){
+    try{
+        const page = await notion.pages.retrieve({page_id: pageId});
+        return page.properties.title.title[0].plain_text;
+    }
+    catch(err){
+        console.log("Error fetching page title:", err);
+    }
 }
 
-getPageContentById(NOTION_PAGE_ID).then((content)=>console.log(content)).catch((error)=>console.log(error));
+//getPageContentById("28e0721d-8085-80fc-80f6-f4f80b9920f5").then((content)=>console.log(content.results[5].paragraph.rich_text)).catch((error)=>console.log(error));
+getPageContentById("28e0721d-8085-80fc-80f6-f4f80b9920f5").then((content)=>console.log(content.results)).catch((error)=>console.log(error));
+getPageTitleById("28e0721d-8085-80fc-80f6-f4f80b9920f5").then((title)=>console.log(title)).catch((error)=>console.log(error));
+// getPageContentById(NOTION_PAGE_ID).then((content)=>console.log(content.results)).catch((error)=>console.log(error));
+// getChildPages(NOTION_PAGE_ID).then((content)=>console.log(content)).catch((error)=>console.log(error));
