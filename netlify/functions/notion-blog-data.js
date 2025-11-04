c2882ed7543fad0097354a68e8e121949fd73a81
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-04 11:05:09 -0500

    working on caching issues on FE

--- netlify/functions/notion-blog-data.js
+++ netlify/functions/notion-blog-data.js
@@ -1,33 +1,28 @@
+import { getStore } from "@netlify/blobs";
 import {config} from "dotenv";
+import Crypto from "crypto";
 config();
 import {savePagesToFile} from "../../utils/helper";
 const NOTION_PAGE_ID = process.env.NOTION_PAGE_ID;
 
 
 
-
 export default async (request, context) => {
     try{
         const blogData =  await savePagesToFile(NOTION_PAGE_ID)
-        return Response.json(blogData, {
-            headers: { 
-                "Cache-Control": "no-store", "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Vary": "Origin", 
-            }
-        });
+        const stringifiedData = JSON.stringify(blogData);
+        const hashKey = Crypto.createHash("sha256").update(stringifiedData).digest("hex");
+        const blogDataStore = getStore({ name: "content", siteID: process.env.NETLIFY_SITE_ID, token: process.env.NETLIFY_ACCESS_TOKEN });
+        const isFound = await blogDataStore.get(hashKey); 
+        if(isFound){
+            return;
+        } else {
+            await blogDataStore.set(hashKey, stringifiedData);
+            await blogDataStore.setJSON("content/manifest.json",{current_key:hashKey,lastUpdated:new Date().toISOString()});
+        }
 
     }
     catch(err){
         console.error("Error fetching blog data:", err);
-        return Response.json({ ok: false, error: err.message }, {
-            headers: { 
-                "Cache-Control": "no-store", "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-                "Vary": "Origin", 
-            }
-        });
     }
 }
